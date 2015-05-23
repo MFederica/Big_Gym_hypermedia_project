441b37397e90877a485d24f5a892da266b3e641e
Paolo <fusari.pool@gmail.com>
2015-05-23 17:01:29 +0200

    added form enroll in course

--- js/course.js
+++ js/course.js
@@ -1,10 +1,10 @@
 $(document).ready(ready);
 
 //variabili globali 
-var storedCourses;//lista dei corsi salvata sul browser del client per il guided tour
-var id;//id del corso da visualizzare passato come parametro
-var course;//campi del database del corso
-var from;//da dove arriva il corso passato come parametro cat-all-basic-medium-advanced
+var storedCourses; //lista dei corsi salvata sul browser del client per il guided tour
+var id; //id del corso da visualizzare passato come parametro
+var course; //campi del database del corso
+var from; //da dove arriva il corso passato come parametro cat-all-basic-medium-advanced
 
 function ready() {
     //recupero lista di corsi per il guided tour
@@ -14,66 +14,80 @@ function ready() {
     id = getQueryVariable("id");
     console.log("corsi da navigare " + storedCourses);
     console.log("corso = " + id + " primo corso = " + storedCourses[0].id + " numero di corsi " + storedCourses.length + "from " + from);
-    //in base a dove arrivo setto i parametri diversi in realtà adesso non è così:)
-    if (from == "cat") {
-        //chiamata ajax per recuperare le informazioni dal data base
-        $.ajax({
-            method: "POST",
-            //passo come parametro l'id del corso da caricare
-            data: {
-                corso: id
-            },
-            crossDomain: true,
-            url: "http://biggympolimi.altervista.org/getCourse.php",
-            success: function (response) {
-                //chiamata ajax ha avuto successo allora carico tutto quello che c'è da caricare nella pagina
-                console.log(JSON.parse(response));
-                course = JSON.parse(response);
-                $(".contiene").html("<div class='course' id='" + course[0].id + "'><h2>" + course[0].name + "</h2><h4>" + course[0].level + "</h2></div>");
-                $(".contiene").html("<div class='course' id='" + course[0].id + "'><h2>" + course[0].name + "</h2><h4>" + course[0].level + "</h2></div>");
 
-                $("#pannello-1 .panel-body").text(course[0].whoi);
-                $("#pannello-2 .panel-body").text(course[0].what);
-                $("#pannello-3 .panel-body").text(course[0].whos);
-                $("#pannello-4 .panel-body").text(course[0].why);
 
-                $(".first-slide").attr("src", "img/" + course[0].img1 + ".jpg");
-                $(".second-slide").attr("src", "img/" + course[0].img2 + ".jpg");
-                var schedule = course[0].schedule.split('|');
-                for (var i = 0; i < schedule.length; i++) {
-                    $('#g' + i).text(schedule[i]);
-                }
-                //cerco il corso all'interno della lista di corsi per il guided tour
-                var pn;//variabile che mi dice nell'array il corso visualizzato per poter fare +1/-1 sul guided tour
-                for (var i = 0; i < storedCourses.length; i++) {
-                    if (storedCourses[i].id == id) {
-                        pn = i;
-                        break;
-                    }
+    //chiamata ajax per recuperare le informazioni dal data base
+    $.ajax({
+        method: "POST",
+        //passo come parametro l'id del corso da caricare
+        data: {
+            corso: id
+        },
+        crossDomain: true,
+        url: "http://biggympolimi.altervista.org/getCourse.php",
+        success: function (response) {
+            //chiamata ajax ha avuto successo allora carico tutto quello che c'è da caricare nella pagina
+            console.log(JSON.parse(response));
+            course = JSON.parse(response);
+            $(".contiene").html("<div class='course' id='" + course[0].id + "'><h2>" + course[0].name + "</h2><h4>" + course[0].level + "</h2></div>");
+            $(".contiene").html("<div class='course' id='" + course[0].id + "'><h2>" + course[0].name + "</h2><h4>" + course[0].level + "</h2></div>");
 
-                }
-                //setto next prev e turn back
-                if (pn == 0) {
+            $("#pannello-1 .panel-body").text(course[0].whoi);
+            $("#pannello-2 .panel-body").text(course[0].what);
+            $("#pannello-3 .panel-body").text(course[0].whos);
+            $("#pannello-4 .panel-body").text(course[0].why);
 
-                } else {
-                    $("#prev").attr("href", "course.html?id=" + storedCourses[+pn - +1].id + "&from=" + from);
+            $(".first-slide").attr("src", "img/" + course[0].img1 + ".jpg");
+            $(".second-slide").attr("src", "img/" + course[0].img2 + ".jpg");
+            var schedule = course[0].schedule.split('|');
+            for (var i = 0; i < schedule.length; i++) {
+                $('#g' + i).text(schedule[i]);
+            }
+            //cerco il corso all'interno della lista di corsi per il guided tour
+            var pn; //variabile che mi dice nell'array il corso visualizzato per poter fare +1/-1 sul guided tour
+            for (var i = 0; i < storedCourses.length; i++) {
+                if (storedCourses[i].id == id) {
+                    pn = i;
+                    break;
                 }
 
-                if (pn == +storedCourses.length - +1) {
+            }
+            //setto parte alta di info
+            $("#namecourse").text(course[0].name + " - " + course[0].level);
+            
 
-                } else {
-                    $("#next").attr("href", "course.html?id=" + storedCourses[+pn + +1].id + "&from=" + from);
-                }
+            //setto next prev e turn back
+            if (pn == 0) {
+
+            } else {
+                $("#prev").attr("href", "course.html?id=" + storedCourses[+pn - +1].id + "&from=" + from);
+            }
+
+            if (pn == +storedCourses.length - +1) {
+
+            } else {
+                $("#next").attr("href", "course.html?id=" + storedCourses[+pn + +1].id + "&from=" + from);
+            }
+
+
+
+            if (from == "cat") {
+                $("#info").text(course[0].category +" courses");
                 $("#back").text("back to " + course[0].category);
                 $("#back").attr("href", "coursepercategory.html?cat=" + course[0].category);
-
-
-            },
-            error: function (request, error) {
-                console.log("Error");
+            } else {
+                $("#info").text(from +" courses");
+                $("#back").text("back to " + from);
+                $("#back").attr("href", "coursepercategory.html?cat=" + course[0].category);
             }
-        });
-    } else {}
+
+
+
+        },
+        error: function (request, error) {
+            console.log("Error");
+        }
+    });
 
 
 
